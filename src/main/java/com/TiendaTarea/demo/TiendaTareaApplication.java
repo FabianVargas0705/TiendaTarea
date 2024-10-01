7b5797ef027404b15c6897140da8b55e05df1890
Fabian Vargas <Fabian Vargas@MED033LW11>
2024-10-01 09:08:42 -0600

    Tarea uno

---
+++ src/main/java/com/TiendaTarea/demo/TiendaTareaApplication.java
@@ -0,0 +1,13 @@
+package com.TiendaTarea.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaTareaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TiendaTarea/demo/TiendaTareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaTarea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
